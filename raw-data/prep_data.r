2d28d59443122a71471278625d5003255a81891b
teng-gao <gaoteng@wustl.edu>
2022-01-29 19:24:06 -0500

    adding raw-data

---
+++ raw-data/prep_data.r
@@ -0,0 +1,8 @@
+genetic_map = fread('~/Eagle_v2.4.1/tables/genetic_map_hg19_withX.txt.gz') %>% 
+    setNames(c('CHROM', 'POS', 'rate', 'cM')) %>%
+    group_by(CHROM) %>%
+    mutate(
+        start = POS,
+        end = c(POS[2:length(POS)], POS[length(POS)])
+    ) %>%
+    ungroup()
