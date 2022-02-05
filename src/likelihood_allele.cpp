9cf36e0fd91817d665a905d1676f2aa1a9ce1baa
evanbiederstedt <evan.biederstedt@gmail.com>
2022-02-05 04:25:16 -0500

    WIP, forward backward compute

--- src/likelihood_allele.cpp
+++ src/likelihood_allele.cpp
@@ -80,3 +80,68 @@ double likelihood_allele_compute(Rcpp::List obj, Rcpp::NumericVector logphi, Rcp
 }
 
 
+// [[Rcpp::export]]
+Rcpp::NumericMatrix forward_backward_compute(Rcpp::List obj, Rcpp::NumericVector logphi, Rcpp::NumericMatrix logprob, Rcpp::List logPi, int n, int m) {
+
+    const int nrow = n;
+    const int ncol = m;
+    Rcpp::NumericMatrix logalpha(nrow, ncol); // logalpha <- matrix(as.double(rep(0, m * n)), nrow = n)
+
+    double lscale = 0.0;
+    double LL = 0.0;
+
+    for (int t = 0; t < n; t++) {
+
+        if (t > 1) {
+            for (int j = 0; j < m; j++) {
+                Rcpp::NumericVector subset_logPi = logPi[t];    
+                Rcpp::NumericVector logphi_logPi = logphi + subset_logPi[j];
+                logphi[j] = logSumExp(logphi_logPi);
+            }
+        }
+
+        logphi = logphi + logprob(t, _);  // Note: logprob(i, _) is Rcpp::NumericVector
+
+        double logSumPhi = logSumExp(logphi);
+
+        logphi = logphi - logSumPhi;
+
+        lscale = lscale + logSumPhi;
+
+        logalpha(t, _) = logphi + lscale;
+    }
+
+    LL = lscale;
+
+    Rcpp::NumericMatrix logbeta(nrow, ncol); // logalpha <- matrix(as.double(rep(0, m * n)), nrow = n)
+    // logphi <- log(as.double(rep(1/m, m)))
+    Rcpp::NumericVector logphi2(m);
+    for (int i = 0; i < m; i++){
+        logphi2[i] = 1/m;
+    }
+    double lscale2 = std::log(m);
+
+    for (int t = n-1; t<=0; t--) {
+        // logphi = sapply(1:m, function(i) matrixStats::logSumExp(logphi + logprob[t+1,] + logPi[[t+1]][i,]))
+
+        for (int i = 0; i < m; i++) {  
+            Rcpp::NumericVector subset_logPi = logPi[t];   
+            Rcpp::NumericVector logphi_logprob_logPi = logphi + logprob(t, _) + subset_logPi[j];
+            logphi[i] = logSumExp(logphi_logprob_logPi);
+        }
+
+        logbeta(t, _) = logphi + lscale;
+
+        double logSumPhi = logSumExp(logphi);
+
+        logphi = logphi - logSumPhi;
+
+        lscale = lscale + logSumPhi;
+    }
+
+    // ??
+    //p_up = exp(logalpha + logbeta - LL)[,1]
+
+    return logalpha;
+}
+
