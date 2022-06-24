b4882ab532bd71069a5451d28f4e1e3d74f9caa6
teng-gao <gaoteng@wustl.edu>
2022-06-23 21:35:32 -0400

    thread safe

--- src/score_tree.cpp
+++ src/score_tree.cpp
@@ -7,26 +7,29 @@ using namespace RcppParallel;
 
 // https://github.com/KlausVigo/phangorn/blob/master/src/phangorn_utils.cpp
 // [[Rcpp::export]]
-List allChildrenCPP(const IntegerMatrix orig) {
-    IntegerVector parent = orig( _, 0);
-    IntegerVector children = orig( _, 1);
+std::vector<std::vector<int>> allChildrenCPP(const arma::Mat<int> E) {
+
+    arma::Col<int> parent = E.col(0);
+    arma::Col<int> children = E.col(1);
     int m = max(parent);
-    // create list for results
-    std::vector< std::vector<int> > out(m);
-    for(int i = 0; i<parent.size(); i++){
-        out[parent[i]-1L].push_back(children[i]);
+
+    std::vector<std::vector<int>> out(m);
+
+    for(int i = 0; i<parent.size(); i++) {
+        out[parent(i)-1L].push_back(children(i));
     }
-    return wrap(out);
+
+    return out;
 }
 
 // [[Rcpp::export]]
-arma::mat CgetQ(arma::mat logQ, List children_dict, IntegerVector node_order){
+arma::mat CgetQ(arma::mat logQ, std::vector<std::vector<int>> children_dict, arma::Col<int> node_order){
 
-    int n = node_order.size();
-    IntegerVector children;
+    int n = node_order.n_rows;
+    std::vector<int> children;
 
     for (int i = 0; i < n; ++i) {
-        int node = node_order[i];
+        int node = node_order(i);
         children = children_dict[node-1];
         logQ.row(node-1) = logQ.row(children[0]-1) + logQ.row(children[1]-1);
     }
@@ -35,57 +38,53 @@ arma::mat CgetQ(arma::mat logQ, List children_dict, IntegerVector node_order){
 }
 
 // [[Rcpp::export]]
-double score_tree_cpp(List tree, arma::mat P) {
+double score_tree_cpp(const arma::Mat<int> E, const arma::mat P) {
 
-    int k = tree["Nnode"];
     int n = P.n_rows;
     int m = P.n_cols;
 
-    arma::mat logQ(k * 2 + 1, m);
+    arma::mat logQ(n * 2 - 1, m);
 
     arma::mat logP_0 = log(P);
     arma::mat logP_1 = log(1-P);
 
     logQ.rows(0, n-1) = logP_1 - logP_0;
 
-    IntegerMatrix E = tree["edge"];
+    arma::Col<int> node_order(E.n_rows + 1);
+    node_order.rows(0,E.n_rows-1) = E.col(1);
+    node_order(E.n_rows) = n+1;
+    arma::uvec ids = find(node_order > n);
+    node_order = node_order.elem(ids);
 
-    IntegerVector node_order = E(_,1);
-    node_order.push_back(n+1);
-    node_order = node_order[node_order > n];
+    std::vector<std::vector<int>> children_dict = allChildrenCPP(E);
 
-    // List children_dict = allChildrenCPP(E);
+    logQ = CgetQ(logQ, children_dict, node_order);
 
-    // logQ = CgetQ(logQ, children_dict, node_order);
+    double l = 0;
 
-    double l_tree = 0;
+    for (int i = 0; i < m; ++i) {
+        l += max(logQ.col(i)) + sum(logP_0.col(i));
+    }
 
-    // for (int i = 0; i < m; ++i) {
-    //     l_tree += max(logQ.col(i)) + sum(logP_0.col(i));
-    // }
-
-    return l_tree;
+    return l;
 }
 
 struct score_nni : public Worker {
 
-    const List trees;
+    const std::vector<arma::Mat<int>> trees_vec;
 
     const arma::mat P;
 
-    NumericVector scores;
+    RVector<double> scores;
 
     // initialize with source and destination
-    score_nni(const List trees, arma::mat P, NumericVector scores):
-        trees(trees), P(P), scores(scores) {}
+    score_nni(const std::vector<arma::Mat<int>> trees_vec, const arma::mat P, NumericVector scores):
+        trees_vec(trees_vec), P(P), scores(scores) {}
 
     // take the square root of the range of elements requested
     void operator()(std::size_t begin, std::size_t end) {
         for (std::size_t i = begin; i < end; i++) {
-            List tree = trees[i];
-            // scores[i] = tree["Nnode"];
-            // scores[i] = score_tree_cpp(tree, P);
-            scores[i] = 0;
+            scores[i] = score_tree_cpp(trees_vec[i], P);
         }
     }
 };
@@ -97,13 +96,24 @@ NumericVector score_nni_parallel(List trees, arma::mat P) {
 
     NumericVector scores(n);
 
-    score_nni score_nni(trees, P, scores);
+    // Convert list to vector of double matrices
+    std::vector<arma::Mat<int>> trees_vec;
+
+    for (int i = 0; i < n; ++i) {
+        List tree = trees[i];
+        arma::Mat<int> E = tree["edge"];
+        trees_vec.push_back(E);
+    }
+
+    score_nni score_nni(trees_vec, P, scores);
 
     parallelFor(0, n, score_nni);
 
     // for (int i = 0; i < n; ++i) {
-    //     List tree = trees[i];
-    //     scores[i] = score_tree_cpp(tree, P);
+    //     // List tree = trees[i];
+    //     // arma::mat E = tree["edge"];
+    //     scores[i] = score_tree_cpp(trees_vec[i], P);
+    //     // scores[i] = n;
     // }
 
     return scores;
