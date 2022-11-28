e39fe5b7a6bd420f0ca3bdbb38e58ef3e49776bf
teng-gao <tgaoteng@gmail.com>
2022-11-27 21:48:59 -0500

    fixing #68

--- R/utils.R
+++ R/utils.R
@@ -507,7 +507,7 @@ analyze_bulk = function(
         stop('Must define diploid region if not given')
     }
 
-    if (is.null(segs_loh)) {
+    if (is.null(segs_loh) & (!'loh' %in% colnames(bulk))) {
         bulk = bulk %>% mutate(loh = FALSE)
     }
 
@@ -583,6 +583,10 @@ analyze_bulk = function(
             mutate(
                 cnv_state_post = ifelse(is.na(cnv_state_post), 'neu', cnv_state_post),
                 cnv_state = ifelse(is.na(cnv_state), 'neu', cnv_state)
+            ) %>%
+            mutate(
+                cnv_state_post = ifelse(loh, 'del', cnv_state_post),
+                cnv_state = ifelse(loh, 'del', cnv_state)
             ) %>%
             mutate(state_post = ifelse(
                 cnv_state_post %in% c('amp', 'del', 'loh') & (!cnv_state %in% c('bamp', 'bdel')),
